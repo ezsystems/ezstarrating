19811e0fb01aab98fca0ee791765326dad3d61df
André Rømcke <ar@ez.no>
2009-09-08 13:40:12 +0000

    Added ger-DE translation contributed by xrow
    git-svn-id: http://svn.projects.ez.no/ezstarrating/trunk@57 618b1747-3c4a-446e-984b-cba050883b41

---
+++ packages/ezstarrating_extension/ezextension/ezstarrating/translations/ger-DE/translation.ts
@@ -0,0 +1,39 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="de_DE" sourcelanguage="en">
+<context>
+    <name>extension/ezstarrating/datatype</name>
+    <message>
+        <location filename="ezsrratingtype.php" line="36"/>
+        <source>Star Rating</source>
+        <comment>Datatype name</comment>
+        <translation>Bewertung mit Sternen</translation>
+    </message>
+    <message>
+        <source>Currently %current_rating og 5 Stars.</source>
+        <translation>Derzeit %current_rating von 5 Sternen.</translation>
+    </message>
+    <message>
+        <source>Rate %rating stars out of 5</source>
+        <translation>Bewertung %rating Sterne von 5</translation>
+    </message>
+    <message>
+        <source>Rating: %current_rating/5</source>
+        <translation>Bewertung: %current_rating/5</translation>
+    </message>
+    <message>
+        <source>%rating_count votes casted</source>
+        <translation>%rating_count Stimmen gezählt</translation>
+    </message>
+    <message>
+        <source>Thank you for rating!</source>
+        <comment>When rating</comment>
+        <translation>Vielen Dank für Ihre Bewertung!</translation>
+    </message>
+    <message>
+        <source>You have already rated this page, you can only rate it once!</source>
+        <comment>When rating</comment>
+        <translation>Sie haben bereits diese Seite bewertet, sie können nur eine Stimme abgeben!</translation>
+    </message>
+</context>
+</TS>
