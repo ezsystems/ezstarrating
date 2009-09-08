21abdb2f0c13d06aeaaa7dae56b1c33a1a4d2956
Gaetano Giunta <gg@ez.no>
2009-09-08 11:50:03 +0000

    - added sql file for Oracle
    git-svn-id: http://svn.projects.ez.no/ezstarrating/trunk@56 618b1747-3c4a-446e-984b-cba050883b41

---
+++ packages/ezstarrating_extension/ezextension/ezstarrating/sql/oracle/oracle.sql
@@ -0,0 +1,30 @@
+CREATE SEQUENCE s_starrating_data;
+
+CREATE TABLE ezstarrating_data (
+  id integer NOT NULL,
+  created_at integer DEFAULT 0 NOT NULL,
+  user_id integer DEFAULT 0 NOT NULL,
+  session_key varchar2(32) NOT NULL,
+  rating double precision DEFAULT 0 NOT NULL,
+  contentobject_id integer DEFAULT 0 NOT NULL,
+  contentobject_attribute_id integer DEFAULT 0 NOT NULL,
+  PRIMARY KEY(id)
+);
+
+CREATE INDEX ezsr_data_user_id_session_key ON ezstarrating_data (user_id, session_key);
+CREATE INDEX ezsr_data_cobj_id_cobj_att_id ON ezstarrating_data (contentobject_id, contentobject_attribute_id);
+
+CREATE TABLE ezstarrating (
+  contentobject_id integer DEFAULT 0 NOT NULL,
+  contentobject_attribute_id integer DEFAULT 0 NOT NULL,
+  rating_average double precision DEFAULT 0 NOT NULL,
+  rating_count integer DEFAULT 0 NOT NULL,
+  PRIMARY KEY (contentobject_id, contentobject_attribute_id)
+);
+
+CREATE OR REPLACE TRIGGER ezstarrating_data_id_tr
+BEFORE INSERT ON ezstarrating_data FOR EACH ROW WHEN (new.id IS NULL)
+BEGIN
+  SELECT s_starrating_data.nextval INTO :new.id FROM dual;
+END;
+/
