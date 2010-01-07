334f3063139f2c43c65e8d8d88a71cdeb3d283ec
Bertrand Dunogier <bd@ez.no>
2010-01-07 14:33:14 +0000

    - Fixed bug #15964 (French translation for ezstarrating extension is missing   Translation file provided by Damien Pobel   (Merged from ezstarrating trunk rev. 92)
    git-svn-id: http://svn.projects.ez.no/ezstarrating/stable/1.0@93 618b1747-3c4a-446e-984b-cba050883b41

---
+++ packages/ezstarrating_extension/ezextension/ezstarrating/translations/fre-FR/translation.ts
@@ -0,0 +1,41 @@
+<!DOCTYPE TS><TS>
+<context>
+    <name>extension/ezstarrating/datatype</name>
+    <message>
+        <source>Star Rating</source>
+        <comment>Datatype name</comment>
+        <translation>Note avec des étoiles</translation>
+    </message>
+    <message>
+        <source>Currently %current_rating out of 5 Stars.</source>
+        <translation>Actuellement %current_rating sur 5 étoiles</translation>
+    </message>
+    <message>
+        <source>Rate %rating stars out of 5</source>
+        <translation>Noter %rating étoiles sur 5</translation>
+    </message>
+    <message>
+        <source>Rating: %current_rating/5</source>
+        <translation>Note : %current_rating/5</translation>
+    </message>
+    <message>
+        <source>%rating_count votes cast</source>
+        <translation>%rating_count notes attribuées</translation>
+    </message>
+    <message>
+        <source>Thank you for rating!</source>
+        <comment>When rating</comment>
+        <translation>Merci d'avoir participé!</translation>
+    </message>
+    <message>
+        <source>You have already rated this page, you can only rate it once!</source>
+        <comment>When rating</comment>
+        <translation>Vous avez déjà noté cette page, vous ne pouvez la noter qu'une fois !</translation>
+    </message>
+    <message>
+        <source>Your rating has been changed, thanks for rating</source>
+        <comment>When rating</comment>
+        <translation>Votre note a été changée, merci</translation>
+    </message>
+</context>
+</TS>
