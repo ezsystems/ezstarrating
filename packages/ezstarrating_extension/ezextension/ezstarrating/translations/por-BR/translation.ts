7144064ce6c197112e92be7d3aa16e2d95110c78
Nicolas Pastorino <nfrp@ez.no>
2010-03-10 11:29:43 +0000

    --Brazilian translation, credits go to Renan Leme
    git-svn-id: http://svn.projects.ez.no/ezstarrating/trunk@105 618b1747-3c4a-446e-984b-cba050883b41

---
+++ packages/ezstarrating_extension/ezextension/ezstarrating/translations/por-BR/translation.ts
@@ -0,0 +1,41 @@
+<!DOCTYPE TS><TS>
+<context>
+    <name>extension/ezstarrating/datatype</name>
+    <message>
+        <source>Star Rating</source>
+        <comment>Datatype name</comment>
+        <translation>Avaliação por estrelas</translation>
+    </message>
+    <message>
+        <source>Currently %current_rating out of 5 Stars.</source>
+        <translation>Atualmente %current_rating de 5 estrelas.</translation>
+    </message>
+    <message>
+        <source>Rate %rating stars out of 5</source>
+        <translation>Avaliado em %rating de 5 estrelas</translation>
+    </message>
+    <message>
+        <source>Rating: %current_rating/5</source>
+        <translation>Avaliado: %current_rating/5</translation>
+    </message>
+    <message>
+        <source>%rating_count votes cast</source>
+        <translation>%rating_count votos realizados</translation>
+    </message>
+    <message>
+        <source>Thank you for rating!</source>
+        <comment>When rating</comment>
+        <translation>Obrigado por avaliar!</translation>
+    </message>
+    <message>
+        <source>You have already rated this page, you can only rate it once!</source>
+        <comment>When rating</comment>
+        <translation>Você já avaliou esta página, você só pode avaliar uma vez!</translation>
+    </message>
+    <message>
+        <source>Your rating has been changed, thanks for rating</source>
+        <comment>When rating</comment>
+        <translation>Sua avaliação foi alterada, obrigado por avaliar</translation>
+    </message>
+</context>
+</TS>
