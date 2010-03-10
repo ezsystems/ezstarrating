cbe3f15500eec64e4bac5be882120705a6d8d7c4
Eric Sagnes <es@ez.no>
2010-03-10 05:32:48 +0000

    Added the Japanese translation
    git-svn-id: http://svn.projects.ez.no/ezstarrating/trunk@104 618b1747-3c4a-446e-984b-cba050883b41

---
+++ packages/ezstarrating_extension/ezextension/ezstarrating/translations/jpn-JP/translation.ts
@@ -0,0 +1,51 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="ja_JP">
+<context>
+    <name>extension/ezstarrating/datatype</name>
+    <message>
+        <source>Star Rating</source>
+        <comment>Datatype name</comment>
+        <translation type="unfinished">スターレーティング</translation>
+    </message>
+    <message>
+        <source>Currently %current_rating out of 5 Stars.</source>
+        <translation type="unfinished">現在の評価は5内で%current_rating。</translation>
+    </message>
+    <message>
+        <source>Rate %rating stars out of 5</source>
+        <translation type="unfinished">5内で評価を付ける</translation>
+    </message>
+    <message>
+        <source>Rating: %current_rating/5</source>
+        <translation type="unfinished">評価:%current_rating/5</translation>
+    </message>
+    <message>
+        <source>%rating_count votes cast</source>
+        <translation type="unfinished">%rating_count投票</translation>
+    </message>
+    <message>
+        <source>Thank you for rating!</source>
+        <comment>When rating</comment>
+        <translation type="unfinished">評価ありがとうございます!</translation>
+    </message>
+    <message>
+        <source>You have already rated this page, you can only rate it once!</source>
+        <comment>When rating</comment>
+        <translation type="unfinished">このページをすでに評価しました、評価は一回しかできないです!</translation>
+    </message>
+    <message>
+        <source>Your rating has been changed, thanks for rating</source>
+        <comment>When rating</comment>
+        <translation type="unfinished">評価を変更しました、ありがとうございます</translation>
+    </message>
+    <message>
+        <source>%login_link_startLog in%login_link_end or %create_link_startcreate a user account%create_link_end to rate this page.</source>
+        <translation type="unfinished">このページを評価するために、%login_link_startログイン%login_link_endか%create_link_startユーザ登録%create_link_endをしてください。</translation>
+    </message>
+    <message>
+        <source>%login_link_startLog in%login_link_end to rate this page.</source>
+        <translation type="unfinished">このページを評価するために、%login_link_startログイン%login_link_endしてください。</translation>
+    </message>
+</context>
+</TS>
