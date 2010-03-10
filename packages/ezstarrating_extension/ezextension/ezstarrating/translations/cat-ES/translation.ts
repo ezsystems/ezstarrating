db21799b4aa4b47dae32bb277559fa0c3dede442
Nicolas Pastorino <nfrp@ez.no>
2010-03-10 12:18:06 +0000

    --Spanish and Catalan translations updated, credits go to Microblau, http://www.microblau.net/
    git-svn-id: http://svn.projects.ez.no/ezstarrating/trunk@106 618b1747-3c4a-446e-984b-cba050883b41

---
+++ packages/ezstarrating_extension/ezextension/ezstarrating/translations/cat-ES/translation.ts
@@ -0,0 +1,64 @@
+<!DOCTYPE TS><TS>
+<context>
+    <name>extension/ezstarrating/datatype</name>
+    <message>
+        <source>Star Rating</source>
+        <comment>Datatype name</comment>
+        <translation>Star Rating</translation>
+    </message>
+    <message>
+        <source>Disabled</source>
+        <translation>Desactivat</translation>
+    </message>
+    <message>
+        <source>Rating: %current_rating/5</source>
+        <translation>Valoració: %current_rating/5</translation>
+    </message>
+    <message>
+        <source>%rating_count votes cast</source>
+        <translation>%rating_count votacions</translation>
+    </message>
+    <message>
+        <source>Currently %current_rating out of 5 Stars.</source>
+        <translation>Actualment %current_rating de 5 Estrelles.</translation>
+    </message>
+    <message>
+        <source>Rate %rating stars out of 5</source>
+        <translation>Valorat %rating stars de 5</translation>
+    </message>
+    <message>
+        <source>disabled</source>
+        <translation>desactivat</translation>
+    </message>
+    <message>
+        <source>Thank you for rating!</source>
+        <comment>When rating</comment>
+        <translation>Gràcies per la teva valoració!</translation>
+    </message>
+    <message>
+        <source>You have already rated this page, you can only rate it once!</source>
+        <comment>When rating</comment>
+        <translation>Ja havies valorat aquesta pàgina, només pots valorar una vegada!</translation>
+    </message>
+    <message>
+        <source>Your rating has been changed, thanks for rating!</source>
+        <comment>When rating</comment>
+        <translation>La teva valoració ha canviat, gràcies per contribuir!</translation>
+    </message>
+    <message>
+        <source>%login_link_startLog in%login_link_end or %create_link_startcreate a user account%create_link_end to rate this page.</source>
+        <translation>%login_link_startConnecta%login_link_end o %create_link_startcrea un compte d&apos;usuari%create_link_end per valorar aquesta pàgina.</translation>
+    </message>
+    <message>
+        <source>%login_link_startLog in%login_link_end to rate this page.</source>
+        <translation>%login_link_startConnecta%login_link_end per valorar aquesta pàgina.</translation>
+    </message>
+</context>
+<context>
+    <name>kernel/content</name>
+    <message>
+        <source>Top Level Nodes</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
---
+++ packages/ezstarrating_extension/ezextension/ezstarrating/translations/esl-ES/translation.ts
@@ -0,0 +1,64 @@
+<!DOCTYPE TS><TS>
+<context>
+    <name>extension/ezstarrating/datatype</name>
+    <message>
+        <source>Star Rating</source>
+        <comment>Datatype name</comment>
+        <translation>Star Rating</translation>
+    </message>
+    <message>
+        <source>Disabled</source>
+        <translation>Desactivado</translation>
+    </message>
+    <message>
+        <source>Rating: %current_rating/5</source>
+        <translation>Valorado: %current_rating/5</translation>
+    </message>
+    <message>
+        <source>%rating_count votes cast</source>
+        <translation>%rating_count valoraciones</translation>
+    </message>
+    <message>
+        <source>Currently %current_rating out of 5 Stars.</source>
+        <translation>Actualmente %current_rating de 5 Estrellas.</translation>
+    </message>
+    <message>
+        <source>Rate %rating stars out of 5</source>
+        <translation>Valorado %rating stars de 5</translation>
+    </message>
+    <message>
+        <source>disabled</source>
+        <translation>desactivado</translation>
+    </message>
+    <message>
+        <source>Thank you for rating!</source>
+        <comment>When rating</comment>
+        <translation>Gracias por tu valoración!</translation>
+    </message>
+    <message>
+        <source>You have already rated this page, you can only rate it once!</source>
+        <comment>When rating</comment>
+        <translation>Ya has valorado esta página, sólo la puedes valorar una vez!</translation>
+    </message>
+    <message>
+        <source>Your rating has been changed, thanks for rating!</source>
+        <comment>When rating</comment>
+        <translation>Tu valoración ha cambiado, gracias por contribuir!</translation>
+    </message>
+    <message>
+        <source>%login_link_startLog in%login_link_end or %create_link_startcreate a user account%create_link_end to rate this page.</source>
+        <translation>%login_link_startConecta%login_link_end o %create_link_startcrea una cuenta de usuario%create_link_end para valorar esta página.</translation>
+    </message>
+    <message>
+        <source>%login_link_startLog in%login_link_end to rate this page.</source>
+        <translation>%login_link_startConecta%login_link_end para valorar esta página.</translation>
+    </message>
+</context>
+<context>
+    <name>kernel/content</name>
+    <message>
+        <source>Top Level Nodes</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
