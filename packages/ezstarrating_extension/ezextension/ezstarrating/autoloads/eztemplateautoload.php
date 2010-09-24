ffb86cd4a96e18595822bb1030cebfc6d4cc682c
André Rømcke <ar@ez.no>
2010-09-24 13:25:12 +0000

    - missing license header
    git-svn-id: http://svn.projects.ez.no/ezstarrating/trunk@121 618b1747-3c4a-446e-984b-cba050883b41

--- packages/ezstarrating_extension/ezextension/ezstarrating/autoloads/eztemplateautoload.php
+++ packages/ezstarrating_extension/ezextension/ezstarrating/autoloads/eztemplateautoload.php
@@ -1,4 +1,11 @@
 <?php
+/**
+ * Template autoload definition for eZ Starrating
+ *
+ * @copyright Copyright (C) 1999-2010 eZ Systems AS. All rights reserved.
+ * @license http://ez.no/licenses/gnu_gpl GNU GPLv2
+ *
+ */
 
 $eZTemplateOperatorArray = array();
 
