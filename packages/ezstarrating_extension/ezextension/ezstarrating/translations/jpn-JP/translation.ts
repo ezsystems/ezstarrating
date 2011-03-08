71f0d9cfa880733540f15582f3c3cdbd123cad23
Eric Sagnes <es@ez.no>
2011-03-08 16:44:26 +0800

    localization: updated jpn-JP translation.ts

--- packages/ezstarrating_extension/ezextension/ezstarrating/translations/jpn-JP/translation.ts
+++ packages/ezstarrating_extension/ezextension/ezstarrating/translations/jpn-JP/translation.ts
@@ -6,46 +6,65 @@
     <message>
         <source>Star Rating</source>
         <comment>Datatype name</comment>
-        <translation type="unfinished">スターレーティング</translation>
+        <translation>スターレーティング</translation>
     </message>
     <message>
         <source>Currently %current_rating out of 5 Stars.</source>
-        <translation type="unfinished">現在の評価は5内で%current_rating。</translation>
+        <translation>現在の評価は5内で%current_rating。</translation>
     </message>
     <message>
         <source>Rate %rating stars out of 5</source>
-        <translation type="unfinished">5内で評価を付ける</translation>
+        <translation>5内で評価を付ける</translation>
     </message>
     <message>
         <source>Rating: %current_rating/5</source>
-        <translation type="unfinished">評価:%current_rating/5</translation>
+        <translation>評価:%current_rating/5</translation>
     </message>
     <message>
         <source>%rating_count votes cast</source>
-        <translation type="unfinished">%rating_count投票</translation>
+        <translation>%rating_count投票</translation>
     </message>
     <message>
         <source>Thank you for rating!</source>
         <comment>When rating</comment>
-        <translation type="unfinished">評価ありがとうございます!</translation>
+        <translation>評価ありがとうございます!</translation>
     </message>
     <message>
         <source>You have already rated this page, you can only rate it once!</source>
         <comment>When rating</comment>
-        <translation type="unfinished">このページをすでに評価しました、評価は一回しかできないです!</translation>
-    </message>
-    <message>
-        <source>Your rating has been changed, thanks for rating</source>
-        <comment>When rating</comment>
-        <translation type="unfinished">評価を変更しました、ありがとうございます</translation>
+        <translation>このページをすでに評価しました、評価は一回しかできないです!</translation>
     </message>
     <message>
         <source>%login_link_startLog in%login_link_end or %create_link_startcreate a user account%create_link_end to rate this page.</source>
-        <translation type="unfinished">このページを評価するために、%login_link_startログイン%login_link_endか%create_link_startユーザ登録%create_link_endをしてください。</translation>
+        <translation>このページを評価するために、%login_link_startログイン%login_link_endか%create_link_startユーザ登録%create_link_endをしてください。</translation>
     </message>
     <message>
         <source>%login_link_startLog in%login_link_end to rate this page.</source>
-        <translation type="unfinished">このページを評価するために、%login_link_startログイン%login_link_endしてください。</translation>
+        <translation>このページを評価するために、%login_link_startログイン%login_link_endしてください。</translation>
+    </message>
+    <message>
+        <source>Disabled</source>
+        <translation>無効</translation>
+    </message>
+    <message>
+        <source>disabled</source>
+        <translation>無効</translation>
+    </message>
+    <message>
+        <source>Your rating has been changed, thanks for rating!</source>
+        <comment>When rating</comment>
+        <translation>評価を変更しました、評価ありがとうございます!</translation>
+    </message>
+    <message>
+        <source>You don&apos;t have access to rate this page.</source>
+        <translation>このページを評価できる権限がありません。</translation>
+    </message>
+</context>
+<context>
+    <name>kernel/content</name>
+    <message>
+        <source>Top Level Nodes</source>
+        <translation>トップレベルノード</translation>
     </message>
 </context>
 </TS>
