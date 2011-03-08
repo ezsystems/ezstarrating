b8d83d8dff57df9aec0c5cb8ca1044a3b0a29fec
Damien Pobel <dpobel@free.fr>
2011-03-09 02:15:25 +0800

    Localization fre-FR : added missing translations

--- packages/ezstarrating_extension/ezextension/ezstarrating/translations/fre-FR/translation.ts
+++ packages/ezstarrating_extension/ezextension/ezstarrating/translations/fre-FR/translation.ts
@@ -1,4 +1,6 @@
-<!DOCTYPE TS><TS>
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0">
 <context>
     <name>extension/ezstarrating/datatype</name>
     <message>
@@ -25,25 +27,49 @@
     <message>
         <source>Thank you for rating!</source>
         <comment>When rating</comment>
-        <translation>Merci d'avoir participé!</translation>
+        <translation>Merci d&apos;avoir participé !</translation>
     </message>
     <message>
         <source>You have already rated this page, you can only rate it once!</source>
         <comment>When rating</comment>
-        <translation>Vous avez déjà noté cette page, vous ne pouvez la noter qu'une fois !</translation>
+        <translation>Vous avez déjà noté cette page, vous ne pouvez la noter qu&apos;une fois !</translation>
     </message>
     <message>
         <source>Your rating has been changed, thanks for rating</source>
         <comment>When rating</comment>
-        <translation>Votre note a été changée, merci</translation>
+        <translation type="obsolete">Votre note a été changée, merci</translation>
     </message>
     <message>
         <source>%login_link_startLog in%login_link_end or %create_link_startcreate a user account%create_link_end to rate this page.</source>
-        <translation type="unfinished"></translation>
+        <translation>%login_link_startIdentifiez vous%login_link_end ou %create_link_startenregistrez vous%create_link_end pour noter cette page.</translation>
     </message>
     <message>
         <source>%login_link_startLog in%login_link_end to rate this page.</source>
-        <translation type="unfinished"></translation>
+        <translation>%login_link_startIdentifiez vous%login_link_end pour noter cette page.</translation>
+    </message>
+    <message>
+        <source>Disabled</source>
+        <translation>Désactivé</translation>
+    </message>
+    <message>
+        <source>disabled</source>
+        <translation>désactivé</translation>
+    </message>
+    <message>
+        <source>Your rating has been changed, thanks for rating!</source>
+        <comment>When rating</comment>
+        <translation>Votre note a été changée, merci de votre participation !</translation>
+    </message>
+    <message>
+        <source>You don&apos;t have access to rate this page.</source>
+        <translation>Vous n'avez pas le droit de noter cette page.</translation>
+    </message>
+</context>
+<context>
+    <name>kernel/content</name>
+    <message>
+        <source>Top Level Nodes</source>
+        <translation>Nœud de premier niveau</translation>
     </message>
 </context>
 </TS>
