aef2a2958d9a65a72b271e7873c0e2186b2746a0
Sandra Parente <sandra@netbliss.it>
2011-11-08 17:28:58 +0100

    Translated in Italian

---
+++ packages/ezstarrating_extension/ezextension/ezstarrating/translations/ita-IT/translation.ts
@@ -0,0 +1,70 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="it_IT">
+<context>
+    <name>extension/ezstarrating/datatype</name>
+    <message>
+        <source>Star Rating</source>
+        <comment>Datatype name</comment>
+        <translation>Star Rating</translation>
+    </message>
+    <message>
+        <source>Currently %current_rating out of 5 Stars.</source>
+        <translation>Attualmente %current_rating su 5 Stelle.</translation>
+    </message>
+    <message>
+        <source>Rate %rating stars out of 5</source>
+        <translation>Voto %rating stelle su 5</translation>
+    </message>
+    <message>
+        <source>Rating: %current_rating/5</source>
+        <translation>Votazione: %current_rating/5</translation>
+    </message>
+    <message>
+        <source>%rating_count votes cast</source>
+        <translation>%rating_count somma dei voti</translation>
+    </message>
+    <message>
+        <source>Thank you for rating!</source>
+        <comment>When rating</comment>
+        <translation>Grazie per il tuo voto!</translation>
+    </message>
+    <message>
+        <source>You have already rated this page, you can only rate it once!</source>
+        <comment>When rating</comment>
+        <translation>Hai già votato per questa pagina, puoi votarla solo una volta!</translation>
+    </message>
+    <message>
+        <source>%login_link_startLog in%login_link_end or %create_link_startcreate a user account%create_link_end to rate this page.</source>
+        <translation>%login_link_startLog in%login_link_end o %create_link_startcrea un account%create_link_end per votare questa pagina.</translation>
+    </message>
+    <message>
+        <source>%login_link_startLog in%login_link_end to rate this page.</source>
+        <translation>%login_link_startLog in%login_link_end per votare questa pagina.</translation>
+    </message>
+    <message>
+        <source>Disabled</source>
+        <translation>Disabilitato</translation>
+    </message>
+    <message>
+        <source>disabled</source>
+        <translation>disabilitato</translation>
+    </message>
+    <message>
+        <source>Your rating has been changed, thanks for rating!</source>
+        <comment>When rating</comment>
+        <translation>Il tuo voto è cambiato, grazie mille!</translation>
+    </message>
+    <message>
+        <source>You don&apos;t have access to rate this page.</source>
+        <translation>Non sei abilitato a votare questa pagina.</translation>
+    </message>
+</context>
+<context>
+    <name>kernel/content</name>
+    <message>
+        <source>Top Level Nodes</source>
+        <translation>Nodi di livello principale</translation>
+    </message>
+</context>
+</TS>
