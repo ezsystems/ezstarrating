04c37f3ed3473b0cecefe0955d4ec2eb00f8dc78
Edi Modric <edi.modric@gmail.com>
2011-11-08 19:46:18 +0000

    Add Croatian translation

---
+++ packages/ezstarrating_extension/ezextension/ezstarrating/translations/cro-HR/translation.ts
@@ -0,0 +1,70 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="hr_HR">
+<context>
+    <name>extension/ezstarrating/datatype</name>
+    <message>
+        <source>Star Rating</source>
+        <comment>Datatype name</comment>
+        <translation>Star Rating</translation>
+    </message>
+    <message>
+        <source>Currently %current_rating out of 5 Stars.</source>
+        <translation>Trenutno %current_rating od 5 zvjedica.</translation>
+    </message>
+    <message>
+        <source>Rate %rating stars out of 5</source>
+        <translation>Ocijeni sa %rating od 5</translation>
+    </message>
+    <message>
+        <source>Rating: %current_rating/5</source>
+        <translation>Ocjena: %current_rating/5</translation>
+    </message>
+    <message>
+        <source>%rating_count votes cast</source>
+        <translation>%rating_count glasova</translation>
+    </message>
+    <message>
+        <source>Thank you for rating!</source>
+        <comment>When rating</comment>
+        <translation>Hvala vam za ocjenjivanje!</translation>
+    </message>
+    <message>
+        <source>You have already rated this page, you can only rate it once!</source>
+        <comment>When rating</comment>
+        <translation>Već ste ocijenili ovaj sadržaj, možete to učiniti samo jednom!</translation>
+    </message>
+    <message>
+        <source>%login_link_startLog in%login_link_end or %create_link_startcreate a user account%create_link_end to rate this page.</source>
+        <translation>%login_link_startPrijavite se%login_link_end ili %create_link_startkreirajte korisnički račun%create_link_end da bi mogli ocijeniti ovaj sadržaj.</translation>
+    </message>
+    <message>
+        <source>%login_link_startLog in%login_link_end to rate this page.</source>
+        <translation>%login_link_startPrijavite se%login_link_end da bi mogli ocijeniti ovaj sadržaj.</translation>
+    </message>
+    <message>
+        <source>Disabled</source>
+        <translation>Isključeno</translation>
+    </message>
+    <message>
+        <source>disabled</source>
+        <translation>isključeno</translation>
+    </message>
+    <message>
+        <source>Your rating has been changed, thanks for rating!</source>
+        <comment>When rating</comment>
+        <translation>Vaša ocjena je izmijenjena, hvala na ocjenjivanju!</translation>
+    </message>
+    <message>
+        <source>You don&apos;t have access to rate this page.</source>
+        <translation>Nemate ovlasti za ocjenjivanje ove stranice.</translation>
+    </message>
+</context>
+<context>
+    <name>kernel/content</name>
+    <message>
+        <source>Top Level Nodes</source>
+        <translation>Vršni čvorovi</translation>
+    </message>
+</context>
+</TS>
