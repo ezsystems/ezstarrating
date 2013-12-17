41b9d0f66a5f0376913ae0dff382f58dd105d893
Yannick ROGER <yannick.roger@ez.no>
2013-12-17 14:20:35 +0100

    Fix EZP-22095: eZ Star Rating does not work for anonymous (cherry picked from commit 37efb7c07fb103b2ec45991ec8d879ef98f235a0)

--- packages/ezstarrating_extension/ezextension/ezstarrating/settings/site.ini.append.php
+++ packages/ezstarrating_extension/ezextension/ezstarrating/settings/site.ini.append.php
@@ -16,4 +16,18 @@ UseUserSession=enabled
 # Allows the user to change his rating after he has rated by returning to the page and rate again
 AllowChangeRating=enabled
 
+# Allow anonymous user to rate content.
+#  You also need :
+#    - to give access to anonymous users to starrating ezjscore functions
+#    - to enable the UseUserSession setting to avoid anonymous to be considered as a single individual
+#
+#  Activating this option (along with UseUserSession):
+#    - Will create a session for every anonymous user rating content.
+#    - might allow spamming since anonymous user is only authenticated by its session cookie.
+#
+#  Relation with the Session/ForceStart setting:
+#    If Session/ForceStart is set to enabled: Existing sessions will be used
+#    if Session/ForceStart is set to disabled: New sessions will be created for any user who starts rating content
+AllowAnonymousRating=disabled
+
 */ ?>
