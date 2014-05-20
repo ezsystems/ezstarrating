a71404e2e8b5df4b81f345da74cd04300ca2ddec
André R <ar@ez.no>
2014-05-20 21:59:28 +0200

    Fix EZP-22128: Update copyright info on old headers
    (cherry picked from commit 50b877b8c31947a1388f54ab8f69253a00e097b5)

--- packages/ezstarrating_extension/ezextension/ezstarrating/autoloads/eztemplateautoload.php
+++ packages/ezstarrating_extension/ezextension/ezstarrating/autoloads/eztemplateautoload.php
@@ -2,7 +2,7 @@
 /**
  * Template autoload definition for eZ Starrating
  *
- * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @copyright Copyright (C) eZ Systems AS.
  * @license For full copyright and license information view LICENSE file distributed with this source code.
  *
  */
